7f4c1031147e96f574f9021379a8dc9b812ede67
User0149 <floatingprecision@gmail.com>
2026-01-19 18:20:29 +1100

    made types into interfaces

--- frontend/src/types/types.ts
+++ frontend/src/types/types.ts
@@ -4,13 +4,13 @@ export type Position = [latitude: number, longitude: number];
 
 export type RouteType = 0 | 1 | 2 | 3 | 4;
 
-export type Disruption = {
+export interface Disruption {
     title?: string,
     description?: string,
     url?: string
-};
+}
 
-export type Run = {
+export interface Run {
     run_ref?: string,
     route_type?: RouteType,
     destination_name?: string,
@@ -19,16 +19,16 @@ export type Run = {
         latitude?: number,
         longitude?: number
     }
-};
+}
 
-export type Route = {
+export interface Route {
     route_name?: string,
     route_number?: string,
     route_type?: RouteType,
     route_id?: number
-};
+}
 
-export type Stop = {
+export interface Stop {
     disruption_ids?: number[],
     stop_distance?: number,
     stop_name?: string,
@@ -37,9 +37,9 @@ export type Stop = {
     route_type?: RouteType,
     stop_latitude?: number,
     stop_longitude?: number
-};
+}
 
-export type Departure = {
+export interface Departure {
     stop_id?: number,
     route_id?: number,
     run_ref?: string,
@@ -47,4 +47,4 @@ export type Departure = {
     scheduled_departure_utc?: string,
     estimated_departure_utc?: string | null,
     platform_number?: string
-};
+}
